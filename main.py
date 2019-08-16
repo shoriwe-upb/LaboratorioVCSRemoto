682675229fc79015e3adc2d24d8d24e9df8abda8
Antonio Jose Donis Hung <antoniojosedonishung@gmail.com>
2019-08-15 21:22:58 -0500

    Mi primera modificacion

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
+from math import sqrt
+
 def main():
     a = float(input("number a >>"))
+    b = float(input("number b >>"))
     c = float(input("number c >>"))
+    d = float(input("number d >>"))
 
     print(f"a * c = {a * c}")
     print(f"2 * a = {2 * a}")
+    print(f"b^2 = {b ** 2}")
+    print(f"sqrt(d) = {sqrt(d)}")
 
 if __name__ == "__main__":
     main()
