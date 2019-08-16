aa15a772645277066ab6d4b6b3fc39d076ff8543
Antonio Jose Donis Hung <antoniojosedonishung@gmail.com>
2019-08-15 21:53:06 -0500

    Solucion

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
 from math import sqrt
 
+
+# Quadratic formula
+def solve(a, b, c):
+    d = (b ** 2) - (4 * a * c)
+    if d >= 0:
+        return (-b + sqrt(d)) / (2 * a), (-b - sqrt(d)) / (2 * a)
+    return False
+
+
 def main():
     a = float(input("number a >>"))
     b = float(input("number b >>"))
     c = float(input("number c >>"))
-    d = float(input("number d >>"))
 
-    print(f"a * c = {a * c}")
-    print(f"2 * a = {2 * a}")
-    print(f"b^2 = {b ** 2}")
-    print(f"sqrt(d) = {sqrt(d)}")
+    # Calculate the values of x
+    answer = solve(a, b, c)
+    # Show results
+    if answer:
+        if answer[0] == answer[1]:
+            print(f"x = {answer[0]}")
+        else:
+            print(f"x = {answer[0]}; x = {answer[1]}")
+    else:
+        # Can't get the square root of a negative number
+        print("Solucion imaginaria")
+
 
 if __name__ == "__main__":
     main()
