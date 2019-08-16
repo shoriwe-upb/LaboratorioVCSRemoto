d36ecee2e5658e6fd4a576d6c8cd677011c71e96
Antonio Jose Donis Hung <antoniojosedonishung@gmail.com>
2019-08-15 21:18:46 -0500

    Mi primer commit

---
+++ main.py
@@ -0,0 +1,9 @@
+def main():
+    a = float(input("number a >>"))
+    c = float(input("number c >>"))
+
+    print(f"a * c = {a * c}")
+    print(f"2 * a = {2 * a}")
+
+if __name__ == "__main__":
+    main()
